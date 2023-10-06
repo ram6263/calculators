f253118aa5e8bb5eff2e7d674e654364aeae1176
RAMNARAYAN SANODIYA <sanodiyaramnarayan92@gmail.com>
2023-10-06 10:48:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+let inputs = document.querySelector('input');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML=='A') {
+            string=eval(string);
+            inputs.value=string
+        }
+        else if (e.target.innerHTML == 'C') {
+            string = ""
+            inputs.value = string;
+        }
+        else if(e.target.innerHTML == 'DL'){
+            string=string.substring(0,string.length-1);
+            inputs.value=string;
+        }
+        else {
+            string = string + e.target.innerHTML;
+            inputs.value = string;
+        }
+    })
+})
